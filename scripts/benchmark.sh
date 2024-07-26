c770a090c8e92d14819ff634199d0e517a3bab93
Clement <gh.2lgqz@aleeas.com>
2024-07-26 20:20:48 +0800

    x

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,16 +2,16 @@ git clone https://github.com/clement2026/etcd.git etcd --single-branch --branch 
 
 cd ./etcd && make build tools && chmod -R u+x ./
 
-export OUTPUT_FILE="result.csv"
 export RATIO_LIST="4/1"
 export REPEAT_COUNT=1
 export RUN_COUNT=10000
 export VALUE_SIZE_POWER_RANGE="8 8"
+export CONN_CLI_COUNT_POWER_RANGE="5 5"
 
-echo OUTPUT_FILE=$OUTPUT_FILE
 echo RATIO_LIST=$RATIO_LIST
 echo REPEAT_COUNT=$REPEAT_COUNT
 echo RUN_COUNT=$RUN_COUNT
 echo VALUE_SIZE_POWER_RANGE=$VALUE_SIZE_POWER_RANGE
+echo CONN_CLI_COUNT_POWER_RANGE=$CONN_CLI_COUNT_POWER_RANGE
 
 cd ./tools/rw-heatmaps && ./rw-benchmark.sh
